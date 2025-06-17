ddae9b349745f27defd6c8c0060a3f03b105cba2
Yeom-Seung-Ah <ysa2780@naver.com>
2025-06-17 20:24:48 +0900

    Feat: 푸터 컴포넌트 제작

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-zinc-100 px-6 py-5">
+      <div className="text-[#737373] text-s leading-relaxed">
+        <p className="pb-3 mb-3 font-regular border-b border-[#A1A1A1]">(주)유레카 사업자정보</p>
+        <p className="mb-1">
+          유메이트 (U:Mate)
+          <br />
+          김현우 | 김민석 | 백세진 | 염승아 | 이승우
+        </p>
+        <p className="text-xs mt-2 mb-2">Copyright ⓒ U:Mate. All Rights Reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
