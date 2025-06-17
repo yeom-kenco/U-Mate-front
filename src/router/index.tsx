c89a10049447fa0049f5957b121e433c10df6795
Yeom-Seung-Ah <ysa2780@naver.com>
2025-06-18 01:30:24 +0900

    Feat: 챗봇버튼 테스트용 임시 챗봇메인페이지 구현 및 라우터 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,20 @@
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Default from '../default';
+import ChatbotMain from '../pages/ChatbotMain';
+
+// Footer 테스트용 임시 페이지
+const TempPage = () => <div>Footer 테스트용 페이지입니다</div>;
 
 const router = createHashRouter([
   {
     element: <Default />,
-    children: [{}],
+    children: [
+      {
+        path: '/',
+        element: <TempPage />, // ✅ Outlet 채우기
+      },
+      { path: 'chatbot', element: <ChatbotMain /> },
+    ],
   },
 ]);
 
