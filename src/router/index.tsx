b3e2bd869cdc3143fcb2a7bd0ee97c5675f1ecf2
Yeom-Seung-Ah <ysa2780@naver.com>
2025-06-18 15:56:08 +0900

    feat: 혜택 드롭바 컴포넌트 구현

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,14 +3,15 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Default from '../default';
 import ChatbotMain from '../pages/ChatbotMain';
 import TermsOfUsePage from '../pages/TermsOfUsePage';
-import BenefitList from '../components/BenefitList';
+import BenefitDropBar from '../components/BenefitDropBar';
+import { benefitList } from '../data/benefits';
 
 // 테스트용 임시 페이지
 const TempPage = () => {
   return (
     <div className="py-10">
-      <h1 className="text-xl font-bold mb-4">혜택 리스트</h1>
-      <BenefitList />
+      <BenefitDropBar label="할인 혜택" indexes={[0, 1, 2, 3, 4]} data={benefitList} />
+      <BenefitDropBar label="기본 혜택" indexes={[5, 6, 7, 8, 9]} data={benefitList} />
     </div>
   );
 };
