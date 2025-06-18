c18c9d44e7917dbbd4657f11ac9babe686ccd066
10012 <nas7062@naver.com>
2025-06-18 22:55:58 +0900

    feat : 요금제 카드 구현 large,small

--- src/default.tsx
+++ src/default.tsx
@@ -2,11 +2,12 @@ import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import ChatbotButton from './components/ChatbotButton.tsx';
+import PricingPage from './pages/PricingPage.tsx';
 
 const Default = () => {
   return (
     <>
-      <Header showBackButton={false} showSearch={true} />
+      <Header showBackButton={false} showSearch={false  } />
       <div className="w-[90%] mx-auto">
         <Outlet />
       </div>
