954af9323a1d6ceab517c544e76a1469f8627dbc
sejinbaek <sejin9767@gmail.com>
2025-06-18 12:29:22 +0900

    style: 폰트 사이즈 원래대로 변경

--- src/components/LoginBanner.tsx
+++ src/components/LoginBanner.tsx
@@ -6,12 +6,12 @@ type LoginBannerProps = {
 const LoginBanner = ({ message, onClick }: LoginBannerProps) => {
   return (
     <div
-      className={`w-full flex justify-between items-center gap-2 px-2 py-3 text-white bg-primary`}
+      className={`w-full flex justify-between items-center gap-2 px-3 py-3 text-white bg-primary`}
     >
-      <span className="text-s font-normal mt-[2px]">{message}</span>
+      <span className="text-sm font-normal mt-[2px]">{message}</span>
       <button
         onClick={onClick}
-        className="text-s border font-normal whitespace-nowrap border-white text-white w-fit px-2 py-1 rounded-lg"
+        className="text-sm border font-normal whitespace-nowrap border-white text-white w-fit px-2 py-1 rounded-lg"
       >
         <span className="mt-[2px] inline-block">로그인</span>
       </button>
