33e38caf77bde87a5fa4d9155ae7b83f504f2dd1
Yeom-Seung-Ah <ysa2780@naver.com>
2025-06-18 09:59:35 +0900

    feat: 임시 이용약관 페이지 제작

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,24 @@
+// router/index.tsx
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Default from '../default';
+import TermsOfUsePage from '../pages/TermsOfUsePage';
+
+// Footer 테스트용 임시 페이지
+const TempPage = () => <div>Footer 테스트용 페이지입니다</div>;
 
 const router = createHashRouter([
   {
     element: <Default />,
-    children: [{}],
+    children: [
+      {
+        path: '/',
+        element: <TempPage />, // ✅ Outlet 채우기
+      },
+      {
+        path: '/terms',
+        element: <TermsOfUsePage />,
+      },
+    ],
   },
 ]);
 
