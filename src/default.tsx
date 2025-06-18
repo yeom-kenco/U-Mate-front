4ea60bb862e713ec58d73a5babfbcb68db987dc7
10012 <nas7062@naver.com>
2025-06-18 11:27:38 +0900

    feat:체크박스 구현

--- src/default.tsx
+++ src/default.tsx
@@ -1,13 +1,13 @@
 import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
-
+import CheckBox from './components/ChekBox';
 
 const Default = () => {
   return (
     <>
       <Header />
-      <div className="w-[90%] mx-auto">
+      <div className="w-[90%] mx-auto" className="w-[90%] mx-auto">
         <Outlet />
       </div>
       <Footer />
