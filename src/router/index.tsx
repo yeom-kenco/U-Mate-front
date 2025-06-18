1a3f79d297778e0271aa021a984184b38c9248b9
sejinbaek <sejin9767@gmail.com>
2025-06-18 15:48:33 +0900

    feat: 공통 버튼 컴포넌트 구현
    - variant: fill | outline | ghost | special
    - color: pink | gray | violet | black
    - size: s | m | lg | xl
    - min-width 설정
    - disabled 처리 및 radius 조건 분기
    - special variant(고객센터 전화하기 버튼)일 경우 아이콘(MdCall) 표시

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,9 +3,58 @@ import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Default from '../default';
 import ChatbotMain from '../pages/ChatbotMain';
 import TermsOfUsePage from '../pages/TermsOfUsePage';
+import Button from '../components/Button';
 
 // Footer 테스트용 임시 페이지
-const TempPage = () => <div>Footer 테스트용 페이지입니다</div>;
+const TempPage = () => (
+  <div>
+    <Button variant="outline" color="gray" size="s">
+      outline
+    </Button>
+    <Button variant="outline" color="pink" size="m">
+      outline
+    </Button>
+    <Button variant="outline" color="violet" size="lg">
+      outline
+    </Button>
+    <Button variant="outline" color="pink" size="xl">
+      outline
+    </Button>
+    <br />
+    <br />
+    <Button variant="fill" color="pink" size="s">
+      fill
+    </Button>
+    <Button variant="fill" color="violet" size="m">
+      fill
+    </Button>
+    <Button variant="fill" color="gray" size="lg">
+      fill
+    </Button>
+    <Button variant="fill" color="pink" size="xl">
+      fill
+    </Button>
+    <br />
+    <br />
+    <Button variant="ghost" color="pink" size="s">
+      ghost
+    </Button>
+    <Button variant="ghost" color="gray" size="m">
+      ghost
+    </Button>
+    <Button variant="ghost" color="violet" size="lg">
+      ghost
+    </Button>
+    <Button variant="ghost" color="black" size="xl">
+      ghost
+    </Button>
+    <br />
+    <br />
+    <Button variant="special" size="lg">
+      고객센터 전화걸기
+    </Button>
+  </div>
+);
 
 const router = createHashRouter([
   {
