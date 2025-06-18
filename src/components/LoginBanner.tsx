33872712ce58327f71059608dfe70dba1c385a45
sejinbaek <sejin9767@gmail.com>
2025-06-18 12:01:03 +0900

    feat: 로그인 배너 컴포넌트 구현
    - 메시지와 로그인 버튼을 포함한 로그인 유도 배너 작성
    - 버튼 클릭 시 onClick 핸들러 실행 (나중에 버튼 컴포넌트로 바꿀 예정)
    - 텍스트가 정가운데에 위치할 수 있도록 mt-[2px] 추가

---
+++ src/components/LoginBanner.tsx
@@ -0,0 +1,22 @@
+type LoginBannerProps = {
+  message: string;
+  onClick: () => void;
+};
+
+const LoginBanner = ({ message, onClick }: LoginBannerProps) => {
+  return (
+    <div
+      className={`w-full flex justify-between items-center gap-2 px-2 py-3 rounded text-white bg-primary`}
+    >
+      <span className="text-s font-normal mt-[2px]">{message}</span>
+      <button
+        onClick={onClick}
+        className="text-s border font-normal whitespace-nowrap border-white text-white w-fit px-2 py-1 rounded-lg"
+      >
+        <span className="mt-[2px] inline-block">로그인</span>
+      </button>
+    </div>
+  );
+};
+
+export default LoginBanner;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,9 +2,15 @@
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Default from '../default';
 import TermsOfUsePage from '../pages/TermsOfUsePage';
+import LoginBanner from '../components/Loginbanner';
 
 // Footer 테스트용 임시 페이지
-const TempPage = () => <div>Footer 테스트용 페이지입니다</div>;
+const TempPage = () => (
+  <div>
+    <LoginBanner message="로그인하고 고객님만을 위한 대화를 시작해보세요" />
+    <LoginBanner message="로그인하고 현재 가입조건을 이용하세요" />
+  </div>
+);
 
 const router = createHashRouter([
   {
