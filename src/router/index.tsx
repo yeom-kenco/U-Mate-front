b6041fb5d1eb9a1c09ad86ef90b7d3a491e15196
sejinbaek <sejin9767@gmail.com>
2025-06-18 16:27:11 +0900

    chore: 병합 시 문제가 발생한 문장 제거

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,6 +6,7 @@ import TermsOfUsePage from '../pages/TermsOfUsePage';
 import Button from '../components/Button';
 import BenefitDropBar from '../components/BenefitDropBar';
 import { benefitList } from '../data/benefits';
+import PricingPage from '../pages/PricingPage';
 
 // 테스트용 임시 페이지
 const TempPage = () => {
@@ -13,10 +14,10 @@ const TempPage = () => {
     <div className="py-10">
       <BenefitDropBar label="할인 혜택" indexes={[0, 1, 2, 3, 4]} data={benefitList} />
       <BenefitDropBar label="기본 혜택" indexes={[5, 6, 7, 8, 9]} data={benefitList} />
+      <Button variant="outline" color="gray" size="s">
+        outline
+      </Button>
     </div>
-    <Button variant="outline" color="gray" size="s">
-      outline
-    </Button>
   );
 };
 
