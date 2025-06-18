7ce6196ed2bdeba2f0f00492a66f687aaab93131
sejinbaek <sejin9767@gmail.com>
2025-06-19 00:24:46 +0900

    style: breakpoint 400px -> 380px로 변경

--- src/default.tsx
+++ src/default.tsx
@@ -2,11 +2,13 @@ import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import ChatbotButton from './components/ChatbotButton.tsx';
+import LoginBanner from './components/LoginBanner.tsx';
 
 const Default = () => {
   return (
     <>
       <Header showBackButton={false} showSearch={true} />
+      <LoginBanner type="chatbot" />
       <div className="w-[90%] mx-auto">
         <Outlet />
       </div>
