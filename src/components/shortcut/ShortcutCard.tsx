b0d59847ad30c3fcb68c6fbe00704088ff038982
Yeom-Seung-Ah <ysa2780@naver.com>
2025-06-19 11:37:41 +0900

    design: 바로가기 카드 shadow 수정

--- src/components/shortcut/ShortcutCard.tsx
+++ src/components/shortcut/ShortcutCard.tsx
@@ -12,8 +12,8 @@ interface ShortcutCardProps {
 const ShortcutCard: React.FC<ShortcutCardProps> = ({ icon, title, description, onClick }) => {
   return (
     <div
-      className="bg-purple-100 relative rounded-3xl shadow-md p-6 flex flex-col items-center text-center
-             transition transform duration-150 active:scale-95 hover:shadow-lg cursor-pointer w-[317px] h-[380px]"
+      className="bg-purple-100 relative rounded-3xl shadow-lg p-6 flex flex-col items-center text-center
+             transition transform duration-150 active:scale-95 hover:shadow-lg cursor-pointer w-[310px] h-[380px]"
       onClick={onClick}
     >
       <div className="mb-4">{icon}</div>
