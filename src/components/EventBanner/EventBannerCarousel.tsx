b9a7c58bc83346a6e4240c9c1c133139a78c4f6a
Yeom-Seung-Ah <ysa2780@naver.com>
2025-06-19 21:53:39 +0900

    feat: 이벤트 배너 캐러셀 구현

---
+++ src/components/EventBanner/EventBannerCarousel.tsx
@@ -0,0 +1,45 @@
+// EventBannerCarousel.tsx
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './../../index.css';
+import EventBannerCard from './EventBannerCard';
+
+const images = [
+  '/images/eventBanner/event-1.png',
+  '/images/eventBanner/event-2.png',
+  '/images/eventBanner/event-3.png',
+  '/images/eventBanner/event-4.png',
+];
+
+const EventBannerCarousel = () => {
+  return (
+    <div className="relative w-full flex justify-center">
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        spaceBetween={-2}
+        slidesPerView={'auto'}
+        centeredSlides
+        loop
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{ clickable: true }}
+        className="w-full pb-10" // ✅ 여백 아래로 밀어내기
+      >
+        {images.map((src, i) => (
+          <SwiperSlide key={i} className="!w-[80%] sm:!w-[400px] flex justify-center">
+            <EventBannerCard imageSrc={src} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* 페이지네이션 수동 배치 */}
+      <div className="custom-pagination absolute left-1/2 -translate-x-1/2 mt-4 bottom-[-24px] flex gap-2" />
+    </div>
+  );
+};
+
+export default EventBannerCarousel;
