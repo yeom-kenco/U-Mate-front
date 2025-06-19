58f470db2ad556040ac742766f9a8a49e81ac88a
sejinbaek <sejin9767@gmail.com>
2025-06-19 20:50:11 +0900

    refactor: 중복된 스타일 분리 및 반응형 너비 대응 리팩토링
    - 공통 baseStyle을 추출하여 토스트 타입별 스타일 중복 제거
    - sm 이상에서 w-[500px], 480px 이하에서는 w-[90%] 적용되도록 반응형 대응 추가
    - icon 제거 및 text 스타일 통일
    - 모바일에서 자주 사용하는 뒷배경이  블러 처리된 투명도 50% 정도의 black 타입 추가
    - 적용되지 않는 스타일 정리

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -10,7 +10,10 @@ import LoginPage from '../pages/LoginPage';
 import LoginBanner from '../components/LoginBanner';
 import Modal from '../components/Modal';
 import { HeaderProps } from '../components/Header';
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
+import Button from '../components/Button';
+
+import { ToastContext } from '../context/ToastContext';
 
 // 테스트용 임시 페이지
 const TempPage = () => {
@@ -23,10 +26,23 @@ const TempPage = () => {
     });
   }, []);
 
+  const toastContext = useContext(ToastContext);
+
+  const handleClick = () => {
+    toastContext?.showToast('리뷰가 삭제되었습니다!', 'black');
+  };
+
   return (
     <div className="py-10">
       <BenefitDropBar label="할인 혜택" indexes={[0, 1, 2, 3, 4]} data={benefitList} />
       <BenefitDropBar label="기본 혜택" indexes={[5, 6, 7, 8, 9]} data={benefitList} />
+      <Button onClick={handleClick}>토스트 리뷰 삭제</Button>
+      <Button onClick={() => toastContext?.showToast('비밀번호가 변경되었습니다!', 'success')}>
+        토스트 성공
+      </Button>
+      <Button onClick={() => toastContext?.showToast('비밀번호가 일치하지 않습니다')}>
+        토스트 실패
+      </Button>
     </div>
   );
 };
