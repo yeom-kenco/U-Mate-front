b3d1a071e77bf8e16506f07cbe84a232b7573908
10012 <nas7062@naver.com>
2025-06-20 17:39:10 +0900

    feat : 이용약관 dropdown

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,5 +1,7 @@
 import clsx from 'clsx';
+import { useState } from 'react';
 import { SlArrowRight } from 'react-icons/sl';
+import { agreements } from '../data/agreements';
 
 interface CheckBoxProps {
   id: string;
@@ -16,23 +18,49 @@ const CheckBox = ({
   checked,
   onChange,
 }: CheckBoxProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // 해당 id에 일치하는 설명 데이터만 가져오기
+  const matchedAgreement = agreements.find((a) => a.id === id);
+
   return (
-    <div className="flex justify-between items-center text-m pt-[5px] mb-2 bg-background h-9 w-full">
-      <div className="flex gap-2 items-center pb-2">
-        {/*체크박스  텍스트 */}
-        <div
-          onClick={() => onChange(id, !checked)}
-          className={`w-6 h-6 rounded-md cursor-pointer border 
-            flex items-center justify-center text-s 
-            transition-all duration-150
-            ${checked ? 'bg-pink-500 text-white border-pink-500' : 'bg-white text-gray-400 border-gray-400'}`}
-        >
-          ✔
+    <div className="mb-4 border border-zinc-300 rounded-md">
+      <div className="flex justify-between items-center text-m pt-[5px] mb-2 bg-background h-9 w-full">
+        <div className="flex gap-2 items-center pb-2">
+          <div
+            onClick={() => onChange(id, !checked)}
+            className={`w-6 h-6 rounded-md cursor-pointer border 
+              flex items-center justify-center text-s 
+              transition-all duration-150
+              ${checked ? 'bg-pink-500 text-white border-pink-500' : 'bg-white text-gray-400 border-gray-400'}`}
+          >
+            ✔
+          </div>
+          <p className={clsx('text-sm mt-2', !showButton && 'font-bold')}>{title}</p>
         </div>
-        <p className={clsx('text-sm mt-2', !showButton && 'font-bold')}>{title}</p>
+        {/* 오른쪽: 화살표 */}
+        {showButton && (
+          <SlArrowRight
+            className="w-4 h-4 mr-1 mb-1 cursor-pointer"
+            onClick={() => setIsOpen(!isOpen)}
+          />
+        )}
       </div>
-      {/* 오른쪽: 화살표 */}
-      {showButton && <SlArrowRight className="w-4 h-4 mr-1 mb-1" />}
+
+      {/* 드롭다운 영역: 해당 id만 보여줌 */}
+      <div
+        className={clsx(
+          'transition-all duration-500 overflow-y-scroll px-3 text-xs text-gray-700 ',
+          isOpen ? 'max-h-[250px] py-2 opacity-100' : 'max-h-0 py-0 opacity-0 '
+        )}
+      >
+        {matchedAgreement && (
+          <div>
+            <p className="font-semibold mb-1 text-sm">{matchedAgreement.title}</p>
+            <p className="whitespace-pre-line">{matchedAgreement.descript}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
