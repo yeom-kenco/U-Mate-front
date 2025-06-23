64231afa4f9251d65e3ecdca1da46796939c5e0c
Yeom-Seung-Ah <ysa2780@naver.com>
2025-06-24 00:56:03 +0900

    design: 요금제 info배너 세로 패딩 더 넓게 수정

--- src/components/MainPage/PlanInfoBanner.tsx
+++ src/components/MainPage/PlanInfoBanner.tsx
@@ -9,7 +9,7 @@ interface PlanInfoBannerProps {
 
 const PlanInfoBanner = ({ planName, dataInfo, dataInfoDetail }: PlanInfoBannerProps) => {
   return (
-    <div className="bg-primary text-white rounded-2xl px-5 py-4 max-[400px]:py-5 text-center">
+    <div className="bg-primary text-white rounded-2xl px-5 py-7 max-[400px]:py-5 text-center">
       <p className="text-s mb-4 font-regular max-[400px]:text-xs">내가 사용중인 요금제</p>
       <h3 className="text-xl font-bold whitespace-pre-wrap max-[400px]:text-lg">{planName}</h3>
       <p className="text-m font-regular">
