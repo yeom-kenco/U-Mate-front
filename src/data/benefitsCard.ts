1bcf8a8d25c33663e2b4c8492b8c0f5cfe1caa83
sejinbaek <sejin9767@gmail.com>
2025-06-23 15:16:07 +0900

    chore: benefitCard 데이터에 id 추가

--- src/data/benefitsCard.ts
+++ src/data/benefitsCard.ts
@@ -1,75 +1,90 @@
 export const benefitCards = [
   {
+    id: 1,
     img: '/images/benefitCard/ott.png',
     title: 'OTT 서비스',
     descript: '다양한 할인 혜택',
   },
   {
+    id: 2,
     img: '/images/benefitCard/multi.png',
     title: '멀티팩',
     descript: '최대 29,400원 혜택',
   },
   {
+    id: 3,
     img: '/images/benefitCard/hello.png',
     title: '헬로렌탈 구독',
     descript: '최대 8,800원 혜택',
   },
   {
+    id: 4,
     img: '/images/benefitCard/illy.png',
     title: '일리커피 구독',
     descript: '최대 18,061원 혜택',
   },
   {
+    id: 5,
     img: '/images/benefitCard/sinhan.png',
     title: '신한카드 Air',
     descript: '다양한 할인 혜택',
   },
   {
+    id: 6,
     img: '/images/benefitCard/easy.png',
     title: '우리집건강이 Easy2+',
     descript: '최대 11,000원 혜택',
   },
   {
+    id: 7,
     img: '/images/benefitCard/samsung.png',
     title: '삼성혜택',
     descript: '최대 10,542원 혜택',
   },
   {
+    id: 8,
     img: '/images/benefitCard/pass.png',
     title: '폰고장패스',
     descript: '최대 7,995원 혜택',
   },
   {
+    id: 9,
     img: '/images/benefitCard/dolbom.png',
     title: '우리집돌봄이 Kids',
     descript: '최대 12,100원 혜택',
   },
   {
+    id: 10,
     img: '/images/benefitCard/milie.png',
     title: '밀리의서재',
     descript: '최대 9,900원 혜택',
   },
   {
+    id: 11,
     img: '/images/benefitCard/uplay.png',
     title: '유플레이',
     descript: '최대 9,900원 혜택',
   },
   {
+    id: 12,
     img: '/images/benefitCard/idle.png',
     title: '아이들나라',
     descript: '최대 9,900원 혜택',
   },
   {
+    id: 13,
     img: '/images/benefitCard/vibe.png',
     title: '바이브',
     descript: '최대 8,800원 혜택',
   },
   {
+    id: 14,
     img: '/images/benefitCard/genie.png',
     title: '지니뮤직',
     descript: '최대 9,600원 혜택',
   },
   {
+    id: 15,
     title: '혜택은 필요 없어요',
   },
 ];
