dde76d6a5b352e0aaef4b02ae7a045d7f16ce432
sejinbaek <sejin9767@gmail.com>
2025-06-23 13:05:52 +0900

    refactor: BenefitCard 버튼으로 변경

--- src/components/BenefitCard.tsx
+++ src/components/BenefitCard.tsx
@@ -6,7 +6,7 @@ interface BenefitCardProps {
 
 const BenefitCard = ({ img, title, descript }: BenefitCardProps) => {
   return (
-    <div className="flex flex-col cursor-pointer items-center justify-center w-[110px] max-[400px]:w-[100px] h-[140px] border-2 border-zinc-300 rounded-lg text-center ">
+    <button className="flex flex-col cursor-pointer items-center justify-center w-[110px] max-[400px]:w-[100px] h-[140px] border-2 border-zinc-300 rounded-lg text-center focus:border-pink-500">
       {img && (
         <div className="w-[50px] h-[50px] rounded-full bg-background overflow-hidden flex-shrink-0 ">
           <img src={img} alt={title} className=" w-full h-full object-contain" />
@@ -16,7 +16,7 @@ const BenefitCard = ({ img, title, descript }: BenefitCardProps) => {
         <p className="text-s w-20 font-bold text-zinc-600 leading-tight ">{title}</p>
         <p className="text-s w-16 text-zinc-500  line-clamp-2 leading-tight">{descript}</p>
       </div>
-    </div>
+    </button>
   );
 };
 
