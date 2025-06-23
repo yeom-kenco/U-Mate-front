0853c4351f5913b1bdc43d70f0f39f6d1d8acdf0
sejinbaek <sejin9767@gmail.com>
2025-06-24 00:18:18 +0900

    refactor: 챗봇 버튼보다 위로 올 수 있게 변경

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -25,7 +25,7 @@ const BottomSheet: React.FC<BottomSheetProps> = ({ children, isOpen, onClose, he
   if (!visible && !isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex justify-center items-end ">
+    <div className="fixed inset-0 z-[100] flex justify-center items-end ">
       {/* Backdrop */}
       <div
         className={clsx(
@@ -38,20 +38,20 @@ const BottomSheet: React.FC<BottomSheetProps> = ({ children, isOpen, onClose, he
       {/* Bottom Sheet */}
       <div
         className={clsx(
-          ' relative w-full max-w-screen bg-white z-[999] dark:bg-neutral-900 rounded-t-[20px] shadow-[0_-2px_10px_rgba(0,0,0,0.1)] flex flex-col transition-transform duration-500',
+          ' relative w-full max-w-screen bg-white z-[9999] dark:bg-neutral-900 rounded-t-[20px] shadow-[0_-2px_10px_rgba(0,0,0,0.1)] flex flex-col transition-transform duration-500',
           isOpen ? 'translate-y-0' : 'translate-y-full'
         )}
         style={{ height }} // 받아온 height로 높이 bottomSheet 높이 지정
       >
         {/* Handle */}
         <div className="pt-3 pb-[15px] flex justify-center items-center" onClick={onClose}>
-          <SlArrowDown className="size-5  text-gray-500 " />
+          <SlArrowDown className="size-5 text-zinc-400" />
         </div>
 
         {/* Content */}
         <div
           className={clsx(
-            `flex-1 overflow-y-hidden px-[18px] pb-[34px] flex flex-col gap-7 scrollbar-hidden `,
+            `overflow-y-hidden px-2 pb-8 scrollbar-hidden `,
             height === '700px' && 'overflow-y-scroll'
           )}
         >
