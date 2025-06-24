c55bae641ae8a7ab64716f5a8a4aa9522307f6b6
sejinbaek <sejin9767@gmail.com>
2025-06-24 17:46:25 +0900

    style: 로그인 페이지 max-w 축소 및 이미지 import 방식으로 변경

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,7 +9,12 @@ import FindAccountModal from '../components/Modal/FindAccountModal';
 import { useDispatch, useSelector } from 'react-redux';
 import { closeModal, openModal } from '../store/modalSlice';
 import { RootState } from '../store/store';
-import { setUser } from '../store/userSlice';
+
+import leftCircle from '../../public/images/login/big-circle-2.png';
+import rightCircle from '../../public/images/login/big-circle-1.png';
+import leftU from '../../public/images/login/big-u-1.png';
+import rightU from '../../public/images/login/big-u-3.png';
+
 const LoginPage = () => {
   const setHeaderConfig = useOutletContext<(config: HeaderProps) => void>();
   const [email, setEmail] = useState('');
@@ -83,39 +88,39 @@ const LoginPage = () => {
       <div className="absolute w-full top-[-60px] left-0 h-[300px] md:h-[500px] z-[-1] overflow-hidden">
         {/* 왼쪽 원 - 데스크탑에서 더 큼 */}
         <img
-          src="/images/login/big-circle-2.png"
+          src={leftCircle}
           alt="배경"
           className="absolute top-36 left-[-30px] object-cover w-16 md:top-44 md:w-28 md:left-[-60px]"
         />
         {/* 오른쪽 원 */}
         <img
-          src="/images/login/big-circle-1.png"
+          src={rightCircle}
           alt="배경"
           className="absolute top-36 right-[-70px] w-24 md:w-44 md:top-48 md:right-[-100px] object-cover"
         />
         {/* 왼쪽 유자형 */}
         <img
-          src="/images/login/big-u-1.png"
+          src={leftU}
           alt="배경"
           className="absolute w-32 top-4 left-[15%] rotate-[5deg] md:w-52 md:top-0 md:left-[10%] object-cover"
         />
         {/* 오른쪽 유자형 */}
         <img
-          src="/images/login/big-u-3.png"
+          src={rightU}
           alt="배경"
           className="absolute w-28 top-[80px] left-[55%] md:w-40 md:top-[90px] md:left-[75%] object-cover"
         />
       </div>
       {/* 실제 로그인 내용: 가운데 정렬 */}
-      <div className="relative max-w-[600px] w-[90%] mx-auto z-10 md:mt-[-70px]">
+      <div className="relative max-w-[372px] w-[90%] mx-auto z-10 md:mt-[-70px]">
         {/*배너 */}
         <div className="w-full relative h-72 flex flex-col items-center justify-center z-0  ">
           {/* 텍스트 */}
-          <p className="relative text-xl font-bold mt-24  md:text-[40px]  ">
+          <p className="relative text-xl font-bold mt-24  md:text-4xl">
             <span className="text-pink-500">U: </span>
-            <span>Mate 로그인</span>
+            Mate 로그인
           </p>
-          <p className="relative text-m mt-2 max-[400px]:text-sm md:text-lm ">
+          <p className="relative text-m mt-4 max-[400px]:text-sm md:text-lm ">
             이메일 또는 휴대폰번호로 빠르게 로그인 하세요
           </p>
         </div>
@@ -125,6 +130,7 @@ const LoginPage = () => {
             onChange={ChangeEmail}
             placeholder="이메일 또는 휴대폰 번호"
             error={errors.email}
+            placeholderStyle="text-[16px] max-[400px]:text-sm"
           />
           <InputField
             value={password}
@@ -132,17 +138,18 @@ const LoginPage = () => {
             placeholder="비밀번호"
             type="password"
             error={errors.password}
+            placeholderStyle="text-[16px] max-[400px]:text-sm"
           />
           <Button
             variant="fill"
             size="xl"
             fullWidth
-            className="mt-8 rounded-xl h-16 sm:text-[1.3rem] md:text-lg "
+            className="mt-8 rounded-xl h-16 sm:text-lm w-full"
           >
             로그인
           </Button>
         </form>
-        <div className="flex pt-6 justify-center gap-4 text-sm text-zinc-500 md:text-m lg:text-lm mb-6">
+        <div className="flex pt-8 justify-center gap-4 text-sm text-zinc-500 md:text-m lg:text-m mb-6">
           <span onClick={() => dispatch(openModal())} className="pr-4 border-r border-gray-300">
             아이디 / 비밀번호 찾기
           </span>
