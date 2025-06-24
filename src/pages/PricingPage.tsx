a7127703936f756244a6c35d3d7f78926f7a832a
sejinbaek <sejin9767@gmail.com>
2025-06-24 18:14:27 +0900

    chore: user에 옵셔널 체이닝 추가

--- src/pages/PricingPage.tsx
+++ src/pages/PricingPage.tsx
@@ -195,7 +195,7 @@ const PricingPage = () => {
 
   // 변경하기 확인 버튼 로직 (사용자 요금제 변경 필요)
   const handleChangePlans = async () => {
-    if (user.id === 0 || user.id === null) {
+    if (user?.id === 0 || user?.id === null) {
       toast?.showToast('로그인 후 이용해 주세요', 'black');
       dispatch(closeModal());
       return;
@@ -207,7 +207,7 @@ const PricingPage = () => {
 
     try {
       await updatePlan({
-        userId: user.id,
+        userId: user?.id,
         newPlanId: selectedPlan?.PLAN_ID,
       });
       setModalType('change');
