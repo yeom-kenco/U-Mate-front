417df41b4125673b7070666d6c2ca3367a6833f7
10012 <nas7062@naver.com>
2025-06-24 11:59:09 +0900

    chore : 주석

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -15,13 +15,13 @@ export const verifyEmailCode = (data: CodeCheck) => axiosInstance.post('/checkAu
 
 // 비밀번호 관리
 export const changePassword = (data: ResetProps) => axiosInstance.post('/passwordChange', data);
-export const resetPassword = (data) => axiosInstance.post('/passwordReset', data);
+//export const resetPassword = (data) => axiosInstance.post('/passwordReset', data);
 export const checkPassword = (data) => axiosInstance.post('/passwordCheck', data);
 
 // 계정 관리
 export const checkPhoneDuplicate = (data: string) => axiosInstance.post('/duplicateCheck', data);
 export const findEmailByPhone = (data: string) => axiosInstance.post('/phoneNumberCheck', data);
-export const getUserInfo = (data) => axiosInstance.post('/userInfo', data); // 서버는 '/getUserInfo'인데 명세대로면 '/userInfo'임
+export const getUserInfo = (data) => axiosInstance.post('/userInfo', data);
 export const deleteAccount = (data) => axiosInstance.post('/withDrawal', data);
 
 export const validateToken = () => axiosInstance.get('/tokenCheck');
